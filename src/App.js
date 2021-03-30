7bfadb7abcf8622b81d71096f3684934ea4b665b
praveen005-github <b.g.praveen.pavan@gmail.com>
2021-03-31 01:02:52 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {  useState } from 'react';
 
 function App() {
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const operators = ["+", "-", "*", "/"];
+
+  const [firstNum, setfirstNum] = useState("");
+  const [secondNum, setSecondNum] = useState("");
+  const [oper, setOper] = useState("");
+  const [res, setRes] = useState("");
+
+
+
+  function clickNum(val) {
+    if (oper === "") {
+      setfirstNum(firstNum + val);
+    } else {
+      setSecondNum(secondNum + val);
+    }
+
+  }
+
+  function clickOperation(val) {
+    setOper(val);
+  }
+
+  function solve() {
+    switch (oper) {
+      case "+":
+        setRes(Number(firstNum) + Number(secondNum));
+        break;
+      case "-":
+        setRes(Number(firstNum) - Number(secondNum));
+        break;
+      case "*":
+        setRes(Number(firstNum) * Number(secondNum));
+        break;
+      case "/":
+        setRes(Number(firstNum) / Number(secondNum));
+        break;
+    }
+  }
+  function display(){
+    if(oper === "")
+      return firstNum;
+    else if(res === "")
+      return (firstNum + " "+ oper +" " + secondNum);
+    else 
+    {
+      return (firstNum + " "+oper +" " + secondNum + "="  + res);
+    }
+  }
+
+  function clear(){
+    setOper("");
+    setfirstNum("");
+    setSecondNum("");
+    setRes("");
+  }
+  function storeRes(){
+    setOper("");
+    setfirstNum(res);
+    setSecondNum("");
+    setRes("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className="calci">
+        <div className="display">{display()}</div>
+        <div className="buttons">
+          <div className="left">
+            <div className='nums'>
+              {numbers.map((val, key) => {
+                return <div id="num" onClick={() => { clickNum(val) }}>{val}</div>
+              })}
+            </div>
+            <div className='but'>
+            <div id="ans" onClick={()=>{solve()}}>Ans</div>
+            <div id="clear" onClick={()=>{clear()}}>Clear</div>
+            <div id="store" onClick={()=>{storeRes()}}>Store</div>
+            </div>
+          </div>
+          <div className="right">
+            {operators.map((val, key) => {
+              return <div id="operators" onClick={() => { clickOperation(val) }}>{val}</div>
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
